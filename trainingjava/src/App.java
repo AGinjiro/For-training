7d621467137cbbffb29932f0166ae482c028bf66
ginjiro <ooo.gin@au.com>
2023-06-02 18:18:22 +0900

    初めてのpush

--- trainingjava/src/App.java
+++ trainingjava/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, 赤坂銀次郎!");
+        System.out.println("Hello, 赤坂_銀次郎!");
     }
 }
